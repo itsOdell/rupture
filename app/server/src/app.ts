1e0431ec0b5d7e8981a17bae2d70227986fa0445
itsOdell <abduladilsunnat@gmail.com>
2023-06-18 10:17:10 -0700

    removed seeding

--- app/server/src/app.ts
+++ app/server/src/app.ts
@@ -1,13 +1,10 @@
 import app from "./app.server";
-import { seedDB } from "./config.db";
 import { PORT } from "@rupture/constants";
 import { connect } from "./init.db";
 import { redisConnect } from "./redis/";
 
 async function main(): Promise<void> {
     await connect();
-    // replace seedDB with deleteSeed if you want to delete all the seeded data
-    await seedDB();
     await redisConnect();
     app.listen(PORT, function () {
         console.log(`Server has started on port ${PORT}`);
