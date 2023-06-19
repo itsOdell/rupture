bb030a516a8b4670bcbede6d3f219277f2ff8748
itsOdell <abduladilsunnat@gmail.com>
2023-06-19 03:59:32 -0700

    refactor: delted useless seeding functions

--- packages/scripts/src/seeding.ts
+++ packages/scripts/src/seeding.ts
@@ -1,6 +1,4 @@
 import Media from "@rupture/server/src/media/media.model";
-import User from "@rupture/server/src/user/user.model";
-import Post from "@rupture/server/src/post/post.model";
 import type { Types } from "mongoose";
 
 export async function seedDefaultPfpOrGetId(): Promise<Types.ObjectId> {
@@ -17,38 +15,8 @@ export async function seedDefaultPfpOrGetId(): Promise<Types.ObjectId> {
     return defaultPfp?._id;
 }
 
-export async function seedDefaultUserOrGetId(): Promise<Types.ObjectId> {
-    let defaultUser = await User.findOne({ userName: "test1", email: "test1@gmail.com" });
-
-    if (defaultUser === null) {
-        defaultUser = await new User({
-            firstName: "test",
-            lastName: "test",
-            email: "test1@gmail.com",
-            password: "test123"
-        }).save();
-    }
-
-    return defaultUser?._id;
-}
-
-export async function seedDefaultPostOrGetId(): Promise<Types.ObjectId> {
-    let defaultPost = await Post.findOne({ userId: await seedDefaultUserOrGetId() });
-
-    if (defaultPost === null) {
-        defaultPost = await new Post({
-            userId: await seedDefaultUserOrGetId(),
-            mediaId: await seedDefaultPfpOrGetId()
-        }).save();
-    }
-
-    return defaultPost?._id;
-}
-
 const seedingcripts = {
-    populateWithDefaultPfp: seedDefaultPfpOrGetId,
-    populateWithDefaultUser: seedDefaultUserOrGetId,
-    populateWithDefaultPost: seedDefaultPostOrGetId
+    seedDefaultPfpOrGetId
 };
 
 export default seedingcripts;
