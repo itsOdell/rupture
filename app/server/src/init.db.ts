4967ef25868a49c88842bc07c644be954ccef35a
itsOdell <abduladilsunnat@gmail.com>
2023-06-27 11:50:13 -0700

    refactor: deleted unneeded code

--- app/server/src/init.db.ts
+++ app/server/src/init.db.ts
@@ -1,21 +1,12 @@
 import mongoose from "mongoose";
 import { MONGODB_URI } from "@rupture/constants";
 
-let connection: any = null;
-
 export async function connect(): Promise<void> {
     try {
-        connection = await mongoose.connect(MONGODB_URI);
+        await mongoose.connect(MONGODB_URI);
         console.log("Successfully connected to Mongo Database");
     } catch (error) {
         console.error("Failed to connect to Mongo Database");
         console.error(error);
-    }
-}
-
-export async function disconnect(): Promise<void> {
-    if (connection !== null) {
-        await connection.disconnect();
-        console.log("Disconnected from Mongo Database");
     }
 }
